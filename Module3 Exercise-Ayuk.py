2d2fdc0e7069eefa847f59e0edb068a4b8671eec
ebotstan <100167232+ebotstan@users.noreply.github.com>
2022-03-28 15:51:59 -0400

    Add files via upload

---
+++ Module3 Exercise-Ayuk.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Print cookies and milk are a great dessert.
+
+# In[5]:
+
+
+print("Cookies and milk are a great dessert")
+
+
+# 2. Print out your favorite color and above the code, comment what you are doing.
+
+# In[6]:
+
+
+# Printing out my favorite color
+favorite_color = "blue"
+print(favorite_color)
+
+
+# 3. Print out your favority color again and add an in-line comment.
+
+# In[7]:
+
+
+favorite_color = 'blue' # This is my favvorite color
+print(favorite_color)
+
+
+# 4. Print the sumation of numbers 1 to 10
+
+# In[8]:
+
+
+print((1+2+3+4+5+6+7+8+9+10))
+
+
+# 5. Print what 6 to the 7th power is.
+
+# In[9]:
+
+
+print(6**7)
+
+
+# 6. Print the remainder of 100 divided by 11. 
+
+# In[10]:
+
+
+print(100%11)
+
+
+# 7. What is the result of "Python" + "1"
+
+# In[12]:
+
+
+print("Python" + "1") # answer is Python1
+
+
+# 8. Print out 100 divided by 5
+
+# In[13]:
+
+
+print(100/5)
+
+
+# 9. What is the different between 10/4 and 10//4?  Print out the result
+
+# In[14]:
+
+
+# The first is a division that outputs the answers and the second gives the answer rounded down
+
+print(10/4)
+print(10//4)
+
+
+# 10. Create a variable named foo that stores a string value.
+
+# In[16]:
+
+
+foo = "leopard"
+
+
+# 11. Print foo variable
+
+# In[17]:
+
+
+foo
+
+
+# 12. Create a variable called height and store your height value there.
+# Print a sentence describing your height and use the variable in the sentence as well.
+
+# In[22]:
+
+
+my_height_in_meters = "1.75"
+print("I am "+ my_height_in_meters + " meters tall")
+
+
+# 13. Concate two strings together using variables and print out the result
+
+# In[25]:
+
+
+a = "Blue"
+b = "sky"
+print(a + " " + b)
+
+
+# 14. Use the two different quotes in a string and print out the sentence
+
+# In[4]:
+
+
+opinion = "This is the people's house"
+print(opinion)
+
+
+# 15. Print out Hello World! 7 times each on a new line using only one line of code.
+
+# In[3]:
+
+
+print("Hello World!\n" * 7)
+
+
+# 16. What is the result of "apple" > "Apple"
+
+# In[ ]:
+
+
+print("apple" > "Apple")
+
+# result is "True"
+
+
+# 17. Print out if 25 multiplied by 54 is bigger than 33 multipled by 33
+
+# In[2]:
+
+
+print(25 * 54 > 33 * 33)
+
+
+# 18. What is the result of of 123 == “123”?
+
+# In[5]:
+
+
+print(123 == "123")
+
+
+# 19. Ask a user for width and length of a rectangle. Then compute its area and perimeter.
+
+# In[6]:
+
+
+rec_length = int(input("enter length: "))
+rec_width  = int(input("enter width: "))
+area = rec_length * rec_width
+perimeter = (rec_length + rec_width) * 2
+print(area)
+print(perimeter)
+
+
+# 20. Ask a user for two whole numbers. Then compute their sum and difference.
+
+# In[7]:
+
+
+a = int(input("enter a number: "))
+b = int(input("enter a second number: "))
+print(a + b)
+print(a - b)
+
+
+# In[2]:
+
+
+a = int(input("enter a number: "))
+b = int(input("enter a second number: "))
+print(a-b)
+
+
+# In[ ]:
+
+
+
+
